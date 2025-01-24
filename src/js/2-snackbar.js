2810df020be74229247c445a503ed34ec2cb7bea
ggreeline <yara.konoshenko@gmail.com>
2025-01-24 22:12:27 +0200

    submit 1

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,49 @@
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
+document.querySelector('.form').addEventListener('submit', event => {
+  event.preventDefault();
+
+  const form = event.currentTarget;
+  const delay = parseInt(form.delay.value, 10);
+  const state = form.state.value;
+
+  if (isNaN(delay) || delay < 0) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Please enter a valid positive number for delay.',
+      position: 'topRight',
+    });
+    return;
+  }
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        title: 'Success',
+        message: `Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+
+  form.reset();
+});
